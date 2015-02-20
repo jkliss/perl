709e419d824368a65f9739292109315e8dc20303
jkliss <KAurion@gmx.de>
2015-02-21 00:17:53 +0100

    Neu

--- matrix.pl
+++ matrix.pl
@@ -6,23 +6,52 @@ use warnings;
 open(MATRIX, "blosum62") or die "Datei nicht gefunden\n";
 
 my @rawMAT = <MATRIX>;
-my @MAT;
+my @mat;
+my @head = ();
+my @combined;
+my @rows;
+my %order;
+my $ordercount = 0;
 
+### @rawMAT von # bereinigen
 foreach my $k (@rawMAT)
 {
 	if(substr($k, 0, 1) ne "#")
 	{
-		push(@MAT, $k);
+		push(@mat, $k);
 	}
 }
 
-foreach my $k (@MAT)
+### durchlaufen der Matrix
+for(my $k = 0; $k <= $#mat; $k++)
 {
-	my @splits = split(" ", $k);
-	foreach my $i (@splits)
+	my @splits = split(" ", $mat[$k]);
+	if($k == 0)
 	{
-		print $i;
+		### head Datei mit der ersten Zeile --- jeweils alle Zeichen als 1 Element
+		for(my $i = 0; $i <= $#splits; $i++)
+		{
+			push(@head, $splits[$i]);
+		}
+	}
+	if(!%order)
+	{
+		foreach my $m (@head)
+		{
+			$order{$head[$ordercount]} = $ordercount;
+			$ordercount++;
+		}
+	}
+	else #Alle anderen Zeilen
+	{
+		shift(@splits);
+		push(@rows, \@splits);
 	}
 }
 
 
+#print @{$rows[5]}[7]; #### Beispiel für das Array, 0 ist die Anzahl der Reihen exklusive head
+#                              1 ist die Position im Array inklusive Buchstaben
+
+
+#print @{$rows[$order{"B"}]}[$order{"C"}]  ## Beispiel für das Benutzen des Order Hashes zum zugreifen
